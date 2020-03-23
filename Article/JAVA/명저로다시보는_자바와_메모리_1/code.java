5fcfc137af85f7c51135f6d0e44dca3bb95c42ac
KoEonYack <21300035@handong.edu>
2020-03-23 22:10:13 +0900

    Init 명저로 다시보는 자바와 메모리

---
+++ Article/JAVA/명저로다시보는_자바와_메모리_1/code.java
@@ -0,0 +1,47 @@
+public class Start {
+    public static void main(String[] args){
+        System.out.println("Hello OOP");
+    }
+}
+
+public class Start3 {
+    public static void main(String[] args){
+      int i = 10;
+      int k = 20;
+      
+      if (i == 10){
+          int m = k + 5;
+          k = m;
+      }else{
+          int p = k+10;
+          k = p;
+      }
+
+      // k = m + p;
+    }
+}
+
+
+public class Start4 {
+    public static void main(String[] args){   
+      int k = 5;
+      int m;
+      
+      m = square();
+    }
+  
+  	private static int square(int k) {
+      int result;
+      k = 25;
+      result = k;
+      return result;
+    }
+}
+
+
+public class Start5 {
+    static int share;
+    public static void main(String[] args){
+        share = 55;
+    }
+}
